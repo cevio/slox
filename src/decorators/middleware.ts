48f6108264e325b45ba806936a7383f898a1b731
沈赟杰 <shenyunjie@znrank.com>
2021-03-04 16:57:58 +0800

    add middleware for function component supported

--- src/decorators/middleware.ts
+++ src/decorators/middleware.ts
@@ -1,15 +1,19 @@
-import React, { useMemo } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import { AnnotationDependenciesAutoRegister, ClassMetaCreator } from "../annotates";
 import { TComponent } from "./component";
 import { container } from '..';
 import { TRequest } from '../request';
 
-export function Middleware(component: TComponent) {
-  AnnotationDependenciesAutoRegister(component, container);
-  return ClassMetaCreator.unshift(Middleware.namespace, (props: React.PropsWithChildren<TRequest>) => {
+export function Middleware(component: TComponent | FunctionComponent<TRequest>) {
+  let isIOComponent = false;
+  if (component.prototype && component.prototype.render) {
+    AnnotationDependenciesAutoRegister(component as TComponent, container);
+    isIOComponent = true;
+  }
+  return ClassMetaCreator.unshift(Middleware.namespace, isIOComponent ? (props: React.PropsWithChildren<TRequest>) => {
     const target = useMemo(() => container.get(component), [component]);
     return React.createElement(target.render.bind(target), props, props.children);
-  });
+  } : component);
 }
 
 Middleware.namespace = Symbol('MIDDLEWARE');
