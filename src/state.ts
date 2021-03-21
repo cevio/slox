41ee4d84db264001a9821a7414af6c09a8cfb737
沈赟杰 <shenyunjie@znrank.com>
2021-03-21 16:15:22 +0800

    update

--- src/state.ts
+++ src/state.ts
@@ -5,29 +5,12 @@ import nextTick from "next-tick";
 export function useReactiveState<T>(fn: () => T) {
   const [state, dispatch] = useReducer((state: T, action: T) => action || state, fn());
   useEffect(() => {
-    let job: ReactiveEffect<T>;
-    let ring = false;
-    let unmounting = false;
-    const scheduler = (ob: ReactiveEffect<T>) => {
-      job = ob;
-      if (ring) return;
-      ring = true;
-      nextTick(() => {
-        if (!unmounting) {
-          dispatch(job());
-        }
-        ring = false;
-      });
-    }
     const _effect = effect(fn, {
       lazy: true,
-      scheduler,
+      scheduler: (job: ReactiveEffect<T>) => dispatch(job()),
     });
-    scheduler(_effect);
-    return () => {
-      unmounting = true;
-      stop(_effect);
-    }
+    dispatch(_effect());
+    return () => stop(_effect);
   }, []);
   return state;
 }
