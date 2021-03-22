65e0f8237e4706a8daea798690842e28452d2610
沈赟杰 <shenyunjie@znrank.com>
2021-03-22 15:36:21 +0800

    update

--- src/promise.ts
+++ src/promise.ts
@@ -10,15 +10,11 @@ export function usePromise<T>(
   const [loading, setLoading] = useReducer((state: boolean, action: boolean) => action, false);
   const [result, setResult] = useReducer((state: T, action: T) => action, initialState);
   const [error, setError] = useReducer((state: any, action: any) => action, null);
-  console.log('loading', loading)
   useEffect(() => {
     let cancel: TUnMounted;
     setLoading(true);
     const promise = new Promise<T>((resolve, reject) => cancel = fn(resolve, reject));
-    promise.then(setResult).catch(setError).finally(() => {
-      setLoading(false);
-      console.log('set false', loading)
-    });
+    promise.then(setResult).catch(setError).finally(() => setLoading(false));
     return cancel;
   }, deps);
   return [loading, result, error];
